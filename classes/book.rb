37f918feaee5125a94b3b37a882519ce54bef5c8
EddyShimwa <shedpray16@gmail.com>
2023-03-28 15:29:04 +0200

    fixed linter errors

--- classes/book.rb
+++ classes/book.rb
@@ -1,14 +1,15 @@
 # book class
 class Book
-    attr_accessor :publisher, :cover_state, :name
-    def initialize(name, cover_state, publisher)
-        super(publish_date)
-      @name = name
-      @publisher = publisher
-      @cover_state = cover_state
-    end
+  attr_accessor :publisher, :cover_state, :name
 
-    def can_be_archived?()
-        super || cover_state == 'bad'
-    end
+  def initialize(name, cover_state, publisher)
+    super(publish_date)
+    @name = name
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?()
+    super || cover_state == 'bad'
+  end
 end
--- classes/label.rb
+++ classes/label.rb
@@ -1,16 +1,16 @@
 # label class
 class Label
-    attr_accessor :title, :color,
-    attr_accessor :item, :id
-    def initialize(title, color, items)
-        @id = Random.rand(1..1000)
-        @title = title
-        @color = color
-        @items = []
-    end
+  attr_accessor :title, :color, :item, :id
 
-    def add_item(item)
-      @items << item
-      item.label = self
-    end
+  def initialize(title, color, _items)
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
 end
