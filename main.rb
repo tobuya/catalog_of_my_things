adb3b77ef190648a7305e22afd4785afdf254aad
EddyShimwa <shedpray16@gmail.com>
2023-03-28 14:45:10 +0200

    added the main.rb file
    Co-authored-by: tobuya <thomasobuya95@gmail.com>
    Co-authored-by: Ridwanullahi-code <ridwanullahiajayi01@gmail.com>

--- main.rb
+++ main.rb
@@ -1,1 +1,19 @@
-# For main.rb
+require_relative 'main/console'
+require_relative 'main/options'
+
+class Main
+  def start
+    puts 'Catalog Of My Things!'
+    puts
+    display = DisplayMenu.new
+    choose_option = ChooseOption.new
+    loop do
+      display.print_menu
+      option = gets.chomp.to_i
+      choose_option.choose_option(option)
+    end
+  end
+end
+
+main = Main.new
+main.start
