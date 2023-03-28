35c0b2048eca3d68e591b13bbc272335ac77b770
tobuya <thomasobuya95@gmail.com>
2023-03-28 12:42:17 +0300

    Create main.rb file that will serve as the console app entry-point
    Co-authored-by: EddyShimwa <shimwaprayeddy@gmail.com>
    Co-authored-by: Ridwanullahi-code <ridwanullahiajayi01@gmail.com>

---
+++ main/console.rb
@@ -0,0 +1,16 @@
+class DisplayMenu
+  def print_menu
+    puts 'Please choose an option by entering a number:'
+    puts '1 - Add a book'
+    puts '2 - List all books'
+    puts '3 - List all labels'
+    puts '4 - Add a music album'
+    puts '5 - List all music albums'
+    puts '6 - List all genres'
+    puts '7 - Add a game'
+    puts '8 - List of games'
+    puts '9 - List all authors'
+    puts '10 - Exit'
+    print 'Enter your choice: '
+  end
+end
