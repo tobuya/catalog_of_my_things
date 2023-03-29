1095ea28ca18582c4b0a9c6b68484988d1b9730b
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2023-03-29 18:29:14 +0100

    add methods to add and list all games and author

--- app.rb
+++ app.rb
@@ -1,18 +1,14 @@
-require_relative 'classes/author'
-require_relative 'classes/book'
-require_relative 'classes/game'
-require_relative 'classes/genre'
-require_relative 'classes/label'
-require_relative 'classes/music_album'
+require_relative './classes/author'
+require_relative './classes/book'
+require_relative './classes/game'
+require_relative './classes/genre'
+require_relative './classes/label'
+require_relative './classes/music_album'
 
 class App
   def initialize
-    @book = Book.new
-    @label = Label.new
-    @music_album = MusicAlbum.new
-    @genre = Genre.new
-    @game = Game.new
-    @author = Author.new
+    @games = []
+    @authors = []
   end
 
   def add_book
@@ -40,15 +36,45 @@ class App
   end
 
   def add_a_game
-    @game.add_a_game
+    print 'Enter the publish date of game [YYYY-MM-DD]'
+    publish_date = gets.chomp.upcase
+    print 'Is it a multiplayer game [Y/N]: '
+    multiplayer = gets.chomp
+    case multiplayer
+    when 'Y'
+      multiplayer = true
+    when 'N'
+      multiplayer = false
+    end
+    print 'What is the last played date? [YYYY-MM-DD]: '
+    last_played_at = gets.chomp.upcase
+    game = Game.new(publish_date, multiplayer, last_played_at)
+    @games << game
+    print 'Enter the author first name: '
+    fname = gets.chomp
+    print 'Enter the author last name: '
+    lname = gets.chomp
+    new_author = Author.new(fname, lname)
+    @authors << new_author
+    puts 'Author added for the Game successfully 🤹‍♂️✅ '
   end
 
   def list_of_games
-    @game.list_of_games
+    return puts 'No Games to Show 🚫 Please add some Games . . .' if @games.empty?
+
+    @games.each_with_index do |game, index|
+      puts "#{index + 1} Multiplayer: #{game.multiplayer} | Last Played: #{game.last_played_at}"
+    end
   end
 
   def list_all_authors
-    @author.list_all_authors
+    if @authors.empty?
+      puts 'No author to Show 🚫 Please add some Authors . . .'
+    else
+      @authors.each_with_index do |author, index|
+        puts "#{index + 1} First Name: #{author.first_name} | Last Name: #{author.last_name}"
+      end
+    end
   end
 
   def invalid_input
