c127f7d694e64f2df7b2d12b51df7cb723ffff6b
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2023-03-30 15:38:23 +0100

    complete testing

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,17 @@
-# for testing
+require_relative '../classes/game'
+require 'time'
+
+
+describe Game do
+  context 'When testing the Game class' do
+    game = Game.new(20, '2020-10-02', 'Y', '2020-02-10')
+
+    it 'Should check if game is multiplayer Y' do
+      expect(game.multiplayer).to eq 'Y'
+    end
+
+    it 'should detect the date the album was published' do
+      expect(Date.parse(game.publish_date)).to eq Date.parse('2020-10-02')
+    end
+  end
+end
