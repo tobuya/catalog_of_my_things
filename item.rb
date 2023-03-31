16ba269bb0b80ce61d448eaf5a0a6be8f81eb230
tobuya <thomasobuya95@gmail.com>
2023-03-31 11:13:49 +0300

    Add add_label method

--- item.rb
+++ item.rb
@@ -17,6 +17,11 @@ class Item
     @archived = true if can_be_archived?
   end
 
+  def add_label(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
+  end
+
   def add_genre(genre)
     @genre = genre
     genre.items << self unless genre.items.include?(self)
